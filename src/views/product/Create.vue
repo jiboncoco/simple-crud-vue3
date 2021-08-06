a643cc08210042629a4373d72ac817dd1f2498a1
Art By Code <artbycode.dev@gmail.com>
2021-08-06 21:47:42 +0700

    update page index & add

--- src/views/product/Create.vue
+++ src/views/product/Create.vue
@@ -1,9 +1,156 @@
 <template>
-    <h1>CREATE</h1>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 rounded shadow">
+                    <div class="card-body">
+                        <h4>Add Product</h4>
+                        <hr>
+
+                        <form @submit.prevent="store">
+                            <div class="form-group">
+                                <label for="name" class="font-weight-bold">Name</label>
+                                <input type="text" class="form-control" v-model="product.name" placeholder="Example for name" required>
+                                <!-- validation -->
+                                <div v-if="validation.name" class="mt-2 alert alert-danger">
+                                    {{ validation.name[0] }}
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="category" class="font-weight-bold">Category</label>
+                                <input type="text" class="form-control" v-model="product.category" placeholder="Example for category" required>
+                                <!-- validation -->
+                                <div v-if="validation.category" class="mt-2 alert alert-danger">
+                                    {{ validation.category[0] }}
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="description" class="font-weight-bold">Description</label>
+                                <textarea class="form-control" rows="4" v-model="product.description" placeholder="Example for description" required></textarea>
+                                <!-- validation -->
+                                <div v-if="validation.description" class="mt-2 alert alert-danger">
+                                    {{ validation.description[0] }}
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="image" class="font-weight-bold">Image</label>
+                                <input type="url" class="form-control" v-model="product.image" placeholder="Example for image" required>
+                                <!-- validation -->
+                                <div v-if="validation.image" class="mt-2 alert alert-danger">
+                                    {{ validation.image[0] }}
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="color" class="font-weight-bold">Color</label>
+                                <input type="text" class="form-control" v-model="product.color" placeholder="Example for color" required>
+                                <!-- validation -->
+                                <div v-if="validation.color" class="mt-2 alert alert-danger">
+                                    {{ validation.color[0] }}
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="size" class="font-weight-bold">Size</label>
+                                <input type="text" class="form-control" v-model="product.size" placeholder="Example for size" required>
+                                <!-- validation -->
+                                <div v-if="validation.size" class="mt-2 alert alert-danger">
+                                    {{ validation.size[0] }}
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="price" class="font-weight-bold">Price</label>
+                                <input type="number" class="form-control" v-model="product.price" placeholder="Example for price" required>
+                                <!-- validation -->
+                                <div v-if="validation.price" class="mt-2 alert alert-danger">
+                                    {{ validation.price[0] }}
+                                </div>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>                        
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
+
 export default {
+
+    setup() {
+
+        //state products
+        const product = reactive({
+            name: '',
+            category: '',
+            description: '',
+            image: '',
+            color: '',
+            size: '',
+            price: ''
+        })
+
+        //state validation
+        const validation = ref([])
+
+        //vue router
+        const router = useRouter()
+
+        //method store
+        function store() {
+
+            let name   = product.name
+            let category = product.category
+            let description   = product.description
+            let image = product.image
+            let color   = product.color
+            let size = product.size
+            let price = product.price
+
+            axios.post('http://localhost:8000/api/product', {
+                name: name,
+                category: category,
+                description: description,
+                image: image,
+                color: color,
+                size: size,
+                price: price
+            }).then(() => {
+
+                //redirect ke product index
+                router.push({
+                    name: 'product.index'
+                })
+
+            }).catch(error => {
+                //assign state validation with error 
+                validation.value = error.response.data
+            })
+
+        }
+
+        //return
+        return {
+            product,
+            validation,
+            router,
+            store
+        }
+
+    }
 
 }
 </script>
+
+<style>
+    body{
+        background: lightgray;
+    }
+</style>
--- src/views/product/Index.vue
+++ src/views/product/Index.vue
@@ -1,9 +1,88 @@
 <template>
-    <h1>INDEX</h1>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 rounded shadow">
+                    <div class="card-body">
+                        <h4>Data Product</h4>
+                        <hr>
+                        <router-link :to="{name: 'product.create'}" class="btn btn-md btn-success">Add</router-link>
+
+                        <table class="table table-striped table-bordered mt-4">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Category</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Image</th>
+                                    <th scope="col">Color</th>
+                                    <th scope="col">Size</th>
+                                    <th scope="col">Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="(product, index) in products" :key="index">
+                                    <td>{{ product.name }}</td>
+                                    <td>{{ product.category }}</td>
+                                    <td>{{ product.description }}</td>
+                                    <td>{{ product.image }}</td>
+                                    <td>{{ product.color }}</td>
+                                    <td>{{ product.size }}</td>
+                                    <td>{{ product.price }}</td>
+                                    <td class="text-center">
+                                        <router-link :to="{name: 'product.edit', params:{id: product.id }}" class="btn btn-sm btn-primary mr-1">EDIT</router-link>
+                                        <button class="btn btn-sm btn-danger ml-1">DELETE</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+
 export default {
+
+    setup() {
+
+        //reactive state
+        let products = ref([])
+
+        //mounted
+        onMounted(() => {
+
+            //get API from Laravel Backend
+            axios.get('http://localhost:8000/api/product')
+            .then(response => {
+              
+              //assign state products with response data
+              products.value = response.data.data
+
+            }).catch(error => {
+                console.log(error.response.data)
+            })
+
+        })
+
+        //return
+        return {
+            products
+        }
+
+    }
 
 }
 </script>
+
+<style>
+    body{
+        background: lightgray;
+    }
+</style>
